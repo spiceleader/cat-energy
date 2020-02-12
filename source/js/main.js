095754f3bcd1ce3836bbaf06ac6dff54b91dc0c0
Rostyslav Miniukov <thresh1337@gmail.com>
2020-02-12 21:43:44 +0200

    Adding logic and hamburger states to site navigation on mobile viewports

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
+// Picture element HTML5 shiv
+document.createElement("picture"); // eslint-disable-line
+
+// Shadow SVG for IE11
 svg4everybody(); // eslint-disable-line
+
+// JavaScript Page Toggler
+const pageBody = document.querySelector('.page');
+if (pageBody.classList.contains('page--no-js')) {
+  pageBody.classList.remove('page--no-js');
+}
+
+// Menu Toggler
+const toggleButton = document.querySelector('.site-nav__toggle');
+const siteNavigation = document.querySelector('.site-nav__list');
+
+if (toggleButton) {
+  toggleButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    siteNavigation.classList.toggle('site-nav__list--closed');
+    this.classList.toggle('site-nav__toggle--opened');
+  });
+}
